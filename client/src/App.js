b196e7165227900af604ee42a7f12a0abc846ba7
defoebrand <defoe.brand@gmail.com>
2020-11-04 10:29:29 +0200

    First initialization of vtc tutorial based on 'coding with chaim' video

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateRoom from "./routes/CreateRoom";
+import Room from "./routes/Room";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={CreateRoom}/>
+            <Route path="/room/:roomID" component={Room}/>
+          </Switch>
+        </BrowserRouter>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
