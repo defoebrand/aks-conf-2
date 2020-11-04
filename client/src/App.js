bbbe5d9f11fd2c0d2d193b46ada1e57537d6806f
defoebrand <defoe.brand@gmail.com>
2020-11-04 18:31:51 +0200

    Add AKS info

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CreateRoom from "./routes/CreateRoom";
@@ -11,20 +11,27 @@ function App() {
       <header className="App-header">
       <h1>Welcome to AKS!</h1>
         <img src={logo} className="App-logo" alt="logo" />
+
+
+        //[DON'T TOUCH THIS]
         <BrowserRouter>
           <Switch>
             <Route path="/" exact component={CreateRoom}/>
             <Route path="/room/:roomID" component={Room}/>
           </Switch>
         </BrowserRouter>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        //DON'T TOUCH THIS
+
+
+        // <a
+        //   className="App-link"
+        //   href="https://reactjs.org"
+        //   target="_blank"
+        //   rel="noopener noreferrer"
+        // >
+        //   Learn React
+        // </a>
+        <a href="https://www.anglustudija.lt/" className="App-link" target="_blank" rel="noopener noreferrer">VISIT AKS!</a>
       </header>
     </div>
   );
