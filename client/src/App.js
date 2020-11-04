2da55096a1f42662a53500cb12f575db170b9847
defoebrand <defoe.brand@gmail.com>
2020-11-04 18:25:48 +0200

    Layout test 2 - add welcome to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Welcome to AKS!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <BrowserRouter>
           <Switch>
