119237e99a3139ef69c5799a0b344b853dfee32a
defoebrand <defoe.brand@gmail.com>
2020-11-05 08:21:45 +0200

    Remove logo when creating room

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { v1 as uuid } from "uuid";
+import {
+  v1 as uuid
+} from "uuid";
 
 const CreateRoom = (props) => {
   function create() {
     const id = uuid();
     props.history.push(`/room/${id}`);
+    const logo = document.querySelector('.App-logo');
+    document.remove(logo);
   }
 
-  return (
-    <button onClick={create}>Create Room</button>
+  return ( <
+    button onClick = {
+      create
+    } > Create Room < /button>
   );
 }
 
